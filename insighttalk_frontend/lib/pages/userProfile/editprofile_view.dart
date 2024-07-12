56cdd6702bb00e59a49dc424e360193063fb14d8
Deepnshu Sharma <deepanshu0859@gmail.com>
2024-07-13 01:11:25 +0530

    Add Category update and created controllers for every field in editProfile

--- insighttalk_frontend/lib/pages/userProfile/editprofile_view.dart
+++ insighttalk_frontend/lib/pages/userProfile/editprofile_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:insighttalk_frontend/router.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 class EditProfileView extends StatefulWidget {
   const EditProfileView({super.key});
@@ -12,202 +13,245 @@ class EditProfileView extends StatefulWidget {
 class _EditProfileViewState extends State<EditProfileView> {
   final _formKey = GlobalKey<FormState>();
   final List<String> _categories = [];
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _emailAddressController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _cityController = TextEditingController();
+  final TextEditingController _stateController = TextEditingController();
+  final TextEditingController _countryController = TextEditingController();
   final TextEditingController _categoryController = TextEditingController();
+  final FocusNode _categoryFocusNode = FocusNode();
+  final List<String> _availableCategories = [
+    'DSA',
+    'Flutter',
+    'Politics',
+    'React',
+    'Cricket',
+    'DSA2',
+    'Flutter2',
+    'Politics2',
+    'React2',
+    'Cricket2',
+  ];
+
+  bool _isHidden = true;
+  void _showpassword() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          // leading: IconButton(
-          //     onPressed: () {},
-          //     icon: const Icon(Icons.arrow_back_ios_new_rounded)),
           title: const Text(
             "Profile",
             textAlign: TextAlign.center,
           ),
         ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(30.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Profile Photo Section
-                Center(
-                  child: Stack(
-                    children: [
-                      Container(
-                        height: 120,
-                        width: 120,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                            image: AssetImage(
-                                'assets/images/blank_profile_pic.jpg'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 0,
-                        right: 0,
-                        child: Container(
-                          width: 35,
-                          height: 35,
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
+        body: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(30.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Profile Photo Section
+                  Center(
+                    child: Stack(
+                      children: [
+                        Container(
+                          height: 120,
+                          width: 120,
+                          decoration: const BoxDecoration(
                             shape: BoxShape.circle,
-                          ),
-                          child: IconButton(
-                            icon: const Icon(
-                              Icons.edit,
-                              color: Colors.white,
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/blank_profile_pic.jpg'),
+                              fit: BoxFit.cover,
                             ),
-                            onPressed: () {
-                              // Handle profile photo edit action
-                            },
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20),
-
-                // Personal Details Section
-                const Text(
-                  'Personal Details',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'User Name',
-                    prefixIcon: Icon(Icons.person),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Email Address',
-                    prefixIcon: Icon(Icons.email),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: 'Password',
-                    prefixIcon: Icon(Icons.lock),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {
-                      // Handle change password action
-                    },
-                    child: const Text('Change Password'),
-                  ),
-                ),
-                const SizedBox(height: 10),
-
-                // Address Details Section
-                const Text(
-                  'Address Details',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'City',
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'State',
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Country',
-                  ),
-                ),
-                const SizedBox(height: 30),
-
-                // Add Category Section
-                const Text(
-                  'Add Category',
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  controller: _categoryController,
-                  decoration: InputDecoration(
-                    hintText: 'Add Category',
-                    suffixIcon: IconButton(
-                      icon: Icon(Icons.add),
-                      onPressed: () {
-                        final category = _categoryController.text;
-                        if (category.isNotEmpty &&
-                            !_categories.contains(category)) {
-                          setState(() {
-                            _categories.add(category);
-                            _categoryController.clear();
-                          });
-                        }
-                      },
+                        Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: Container(
+                            width: 35,
+                            height: 35,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              shape: BoxShape.circle,
+                            ),
+                            child: IconButton(
+                              icon: const Icon(
+                                Icons.edit,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                // Handle profile photo edit action
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  onFieldSubmitted: (value) {
-                    if (value.isNotEmpty && !_categories.contains(value)) {
-                      setState(() {
-                        _categories.add(value);
-                        _categoryController.clear();
-                      });
-                    }
-                  },
-                ),
-                const SizedBox(height: 10),
-                Wrap(
-                  spacing: 8.0,
-                  children: _categories.map((category) {
-                    return Chip(
-                      label: Text(category),
-                      onDeleted: () {
-                        setState(() {
-                          _categories.remove(category);
-                        });
-                      },
-                    );
-                  }).toList(),
-                ),
-                const SizedBox(height: 20),
+                  const SizedBox(height: 20),
 
-                // Save Button
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState?.validate() ?? false) {
-                        // Handle save action
-                        context.goNamed(routeNames.experts);
-                      }
-                    },
-                    child: const Text('Save'),
+                  // Personal Details Section
+                  const Text(
+                    'Personal Details',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    controller: _userNameController,
+                    decoration: const InputDecoration(
+                      hintText: 'User Name',
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    controller: _emailAddressController,
+                    decoration: const InputDecoration(
+                      hintText: 'Email Address',
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    obscureText: _isHidden,
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                        hintText: 'Password',
+                        prefixIcon: const Icon(Icons.lock),
+                        suffixIcon: IconButton(
+                          icon: Icon(_isHidden
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                          onPressed: _showpassword,
+                        )),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {
+                        // Handle change password action
+                      },
+                      child: const Text('Change Password'),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+
+                  // Address Details Section
+                  const Text(
+                    'Address Details',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    controller: _cityController,
+                    decoration: const InputDecoration(
+                      hintText: 'City',
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    controller: _stateController,
+                    decoration: const InputDecoration(
+                      hintText: 'State',
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    controller: _countryController,
+                    decoration: const InputDecoration(
+                      hintText: 'Country',
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+
+                  // Add Category Section
+                  const Text(
+                    'Add Category',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TypeAheadField<String>(
+                    textFieldConfiguration: TextFieldConfiguration(
+                      controller: _categoryController,
+                      focusNode: _categoryFocusNode,
+                      decoration: const InputDecoration(
+                        hintText: 'Search Category',
+                      ),
+                    ),
+                    suggestionsCallback: (pattern) {
+                      return _availableCategories.where((item) =>
+                          item.toLowerCase().startsWith(pattern.toLowerCase()));
+                    },
+                    itemBuilder: (context, suggestion) {
+                      return ListTile(
+                        title: Text(suggestion),
+                      );
+                    },
+                    onSuggestionSelected: (suggestion) {
+                      if (!_categories.contains(suggestion)) {
+                        setState(() {
+                          _categories.add(suggestion);
+                          _availableCategories.remove(suggestion);
+                        });
+                      }
+                      _categoryController.clear();
+                      _categoryFocusNode.unfocus();
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  Wrap(
+                    spacing: 8.0,
+                    children: _categories.map((category) {
+                      return Chip(
+                        label: Text(category),
+                        onDeleted: () {
+                          setState(() {
+                            _categories.remove(category);
+                            _availableCategories.add(category);
+                          });
+                        },
+                      );
+                    }).toList(),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+
+                  // Save Button
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState?.validate() ?? false) {
+                          // Handle save action
+                          context.goNamed(routeNames.experts);
+                        }
+                      },
+                      child: const Text('Save'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
