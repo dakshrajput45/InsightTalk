d619d17d8d25c1bbe82d6e42855dbf8e61c8ab61
Sushant123450 <Sushant123450.sk@gmail.com>
2024-07-20 20:43:42 +0530

    trim added to email in login and sign up

--- insighttalk_frontend/lib/pages/auth/SignUp_view.dart
+++ insighttalk_frontend/lib/pages/auth/SignUp_view.dart
@@ -118,7 +118,7 @@ class _SignUpViewState extends State<SignUpView> {
                             confirmPasswordController.text) {
                           User? user =
                               await _itUserAuthSDK.emailandPasswordSignUp(
-                                  email: emailController.text,
+                                  email: emailController.text.trim(),
                                   password: passwordController.text);
                           if (user != null && mounted) {
                             context.goNamed(routeNames.editprofileview);
@@ -127,8 +127,7 @@ class _SignUpViewState extends State<SignUpView> {
                             print("Sign Up Failed");
                           }
                         } else {
-                          print(
-                              "Password and Confirm Passwords are not same");
+                          print("Password and Confirm Passwords are not same");
                         }
                         // const ProfileScreen();
                       },
@@ -195,8 +194,7 @@ class _SignUpViewState extends State<SignUpView> {
                             WidgetStateProperty.all<Color>(Colors.white),
                         foregroundColor:
                             WidgetStateProperty.all<Color>(Colors.black),
-                        shape:
-                            WidgetStateProperty.all<RoundedRectangleBorder>(
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
                           RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10.0),
                             side: const BorderSide(color: Colors.grey),
